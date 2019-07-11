4573cab86bbe91ccf7ea2f020b65dbc1b563f8b5
heldermarchetto <heldermarchetto@users.noreply.github.com>
2019-07-11 11:38:29 +0200

    Updated example to show also parameter list

--- testReadUview.py
+++ testReadUview.py
@@ -1,21 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 11 11:17:43 2019
-
-@author: Helder
+Copyright 2019 Dr. Helder Marchetto
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 """
 
 import readUview as ru
 import matplotlib.pyplot as plt
 
-fn = r'K:\Data\SMART-2\2019\0507_HM_MP_TS_FU-Berlin\20190507a001.dat'
+fn = r'C:\Insert\Your\Path\To\Your\File\Here\myFile.dat'
 
 ruObj = ru.readUviewClass()
 img = ruObj.getImage(fn)
 
+#show the image
 plt.imshow(img, cmap=plt.cm.gray)
-fn = r'K:\Data\SMART-2\2019\0507_HM_MP_TS_FU-Berlin\20190507b001.dat'
 
+#show the image parameter list
 for p in ruObj.paramList:
     print(p)
 
